35ff41efe33b3e680470b7a248911076168f9208
megowy <megowy@gmail.com>
2023-03-06 18:05:08 +0100

    modyfied models.py, urls.py, views.py

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import loader
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request,  'polls/index.html', context)
 
 
 def detail(request, question_id):
@@ -22,4 +22,4 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    return HttpResponse("You're voting on question %s." % question_id)("You're voting on question %s." % question_id)
